8e27a60297b2a554aecb64be87ca1596c81061c8
Hudson Pace <hudsonmpace@gmail.com>
2022-10-18 15:31:23 -0600

    Make sure first click is safe, track flags.

--- src/Minesweeper.js
+++ src/Minesweeper.js
@@ -22,16 +22,16 @@ const getCoordsFromIndex = (index, width) => {
   }
 }
 
-const countMines = (index, width, arr) => {
+const countMines = (index, width, height, arr) => {
   let mineCount = 0;
-  if (arr[index] === -1) {
+  if (arr[index].value === -1) {
     return -1;
   }
   const {x, y} = getCoordsFromIndex(index, width);
   for (let i = -1; i < 2; i++) {
     for (let j = -1; j < 2; j++) {
-      const index = getIndexFromCoords(x + i, y + j, 10, 10);
-      if (index !== -1 && arr[index] === -1) {
+      const index = getIndexFromCoords(x + i, y + j, width, height);
+      if (index !== -1 && arr[index].value === -1) {
         mineCount++;
       }
     }
@@ -43,21 +43,33 @@ const coordsAreValid = (x, y, width, height) => {
   return (x >= 0 && y >= 0 && x < width && y < height);
 }
 
+const updateMineCounts = (grid, width, height) => {
+  for (let i = 0; i < grid.length; i++) {
+    if (grid[i].value !== -1) {
+      grid[i] = {
+        ...grid[i],
+        value: countMines(i, width, height, grid),
+      }
+    }
+  }
+}
+
 const generateGrid = (width, height, mineCount) => {
   let grid = new Array(width * height).fill(0);
+  grid = grid.map((value) => { return { value }})
   for (let i = 0; i < Math.min(width * height, mineCount); i++) {
-    grid[i] = -1;
+    grid[i].value = -1;
   }
   shuffleArray(grid);
   for (let i = 0; i < grid.length; i++) {
-    if (grid[i] !== -1) {
-      grid[i] = countMines(i, width, grid);
+    if (grid[i].value !== -1) {
+      grid[i].value = countMines(i, width, height, grid);
     }
-  };
+  }
 
   grid = grid.map((tile, index) => {
     return {
-      value: tile,
+      value: tile.value,
       flagged: false,
       exposed: false,
       index
@@ -122,34 +134,81 @@ const showNeighbors = (index, grid, width, height) => {
   }
 }
 
-const handleClick = (e, index, grid, setGrid, width, height) => {
-  if (!grid[index].exposed && !grid[index].flagged) {
-    const newGrid = grid.slice();
-    showTile(index, newGrid, width, height);
-    setGrid(newGrid);
-  } else if (e.buttons === 2 && grid[index].exposed && grid[index].value === countFlagsAroundIndex(index, grid, width, height)) {
-    const newGrid = grid.slice();
-    showNeighbors(index, newGrid, width, height);
-    setGrid(newGrid);
-  }
+const findValidEmptyTile = (x, y, width, height, grid) => {
+  let candidateIndex;
+  let valid;
+  let dx;
+  let dy;
+  const length = width * height;
+  do {
+    candidateIndex = Math.floor(Math.random() * length);
+    const {x: x2, y: y2} = getCoordsFromIndex(candidateIndex, width);
+    valid = coordsAreValid(x2, y2, width, height);
+    dx = Math.abs(x2 - x);
+    dy = Math.abs(y2 - y);
+  } while (!valid || dx < 2 || dy < 2 || grid[candidateIndex].value === -1);
+  return candidateIndex;
 }
-const handleRightClick = (e, index, grid, setGrid) => {
-  e.preventDefault();
-  const newGrid = grid.slice();
-  const tile = {...grid[index]};
-  if (!tile.exposed) {
-    tile.flagged = !tile.flagged;
+
+const clearMinesAroundTile = (index, grid, width, height) => {
+  const {x, y} = getCoordsFromIndex(index, width);
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      const x2 = x + i;
+      const y2 = y + j;
+      if (coordsAreValid(x2, y2, width, height)) {
+        const index2 = getIndexFromCoords(x2, y2, width, height);
+        if (grid[index2].value === -1) {
+          const newIndex = findValidEmptyTile(x, y, width, height, grid);
+          const temp = grid[index2];
+          grid[index2] = {...grid[newIndex], index: index2};
+          grid[newIndex] = {...temp, index: newIndex};
+        }
+      }
+    }
   }
-  newGrid[index] = tile;
+  updateMineCounts(grid, width, height);
+}
+
+const handleClick = (e, index, grid, setGrid, width, height, firstClick, setFirstClick) => {
+  const newGrid = grid.slice();
+  if (firstClick) {
+    setFirstClick(false);
+    if (newGrid[index].value !== 0) {
+      clearMinesAroundTile(index, newGrid, width, height);
+    }
+  }
+  if (!newGrid[index].exposed && !newGrid[index].flagged) {
+    showTile(index, newGrid, width, height);
+  } else if (e.buttons === 2 && newGrid[index].exposed && newGrid[index].value === countFlagsAroundIndex(index, newGrid, width, height)) {
+    showNeighbors(index, newGrid, width, height);
+  }
   setGrid(newGrid);
+}
+const handleRightClick = (e, index, grid, setGrid, flagCount, setFlagCount) => {
+  e.preventDefault();
+  
+  if ((flagCount > 0 || grid[index].flagged) && !grid[index].exposed) {
+    const newGrid = grid.slice();
+    const tile = {...grid[index]};
+    const newCount = flagCount + (tile.flagged ? 1 : -1);
+    tile.flagged = !tile.flagged;
+    newGrid[index] = tile;
+    setGrid(newGrid);
+    setFlagCount(newCount);
+  }
 }
 
 export default function Minesweeper() {
-  const width = 10;
-  const height = 10;
+  const width = 18; // 10;
+  const height = 14; // 10;
+  const mineCount = 40; // 10;
   const [grid, setGrid] = useState(() => {
-    return generateGrid(width, height, 10);
+    return generateGrid(width, height, mineCount);
   });
+  const [firstClick, setFirstClick] = useState(true);
+  const [flagCount, setFlagCount] = useState(mineCount);
+
   const squares = grid.map((tile) => {
     return <Tile
       value={tile.value}
@@ -157,19 +216,22 @@ export default function Minesweeper() {
       flagged={tile.flagged}
       index={tile.index}
       key={tile.index}
-      onClick={(e, index) => handleClick(e, index, grid, setGrid, width, height)}
-      onContextMenu={(e, index) => handleRightClick(e, index, grid, setGrid)}
+      onClick={(e, index) => handleClick(e, index, grid, setGrid, width, height, firstClick, setFirstClick)}
+      onContextMenu={(e, index) => handleRightClick(e, index, grid, setGrid, flagCount, setFlagCount)}
     />
   });
   const rows = [];
-  for (let i = 0; i < 10; i++) {
-    rows.push(<div className="row" key={i}>{squares.slice(i * 10, (i + 1) * 10)}</div>);;
+  for (let i = 0; i < width; i++) {
+    rows.push(<div className="row" key={i}>{squares.slice(i * width, (i + 1) * width)}</div>);;
   }
   return (
     <div>
       <h1>Minesweeper!</h1>
       <div className="game-container">
         {rows}
+      </div>
+      <div>
+        Flags: {flagCount}
       </div>
     </div>
   )
